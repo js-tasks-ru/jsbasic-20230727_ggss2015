d050602f4d7059caa97df334440ed6da0d418168
inuti1e <ggss2015@mail.ru>
2023-08-15 15:40:12 +0300

    cteare camelize function

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,7 @@
 function getMinMax(str) {
-  // ваш код...
+  let elements = str.split(' ');
+  let numbers = elements.map(parseFloat).filter(number => !isNaN(number))
+  const min = Math.min(...numbers);
+  const max = Math.max(...numbers);
+  return { min, max };
 }
