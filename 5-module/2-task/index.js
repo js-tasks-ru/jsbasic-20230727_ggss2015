b58cceadb9b64d19fb14575b3f1657279f09a63c
inuti1e <ggss2015@mail.ru>
2023-08-24 20:35:11 +0300

    help

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
-}
+  const hideButton = document.querySelector('.toggle-text-button');
+  const hideText = document.getElementById('text');
+  let visibleText = true;
+hideButton.addEventListener('click', () => {
+  if (visibleText) {
+    hideText.style.display = 'none';
+  } else {
+    hideText.style.display = 'block';
+  }
+    visibleText = !visibleText;
+})
+};
