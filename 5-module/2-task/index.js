15e4de3def0b900b42e2d306afb07adfd633687b
inuti1e <ggss2015@mail.ru>
2023-08-25 16:53:37 +0300

    corrected

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,13 +1,7 @@
 function toggleText() {
   const hideButton = document.querySelector('.toggle-text-button');
   const hideText = document.getElementById('text');
-  let visibleText = true;
 hideButton.addEventListener('click', () => {
-  if (visibleText) {
-    hideText.style.display = 'none';
-  } else {
-    hideText.style.display = 'block';
-  }
-    visibleText = !visibleText;
+  hideText.hidden = !hideText.hidden;
 })
-};
+}
