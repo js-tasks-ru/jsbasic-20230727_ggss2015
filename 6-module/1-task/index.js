510bdca7f328d836a3c5dd782141482ab6c7c93b
inuti1e <ggss2015@mail.ru>
2023-08-25 16:43:42 +0300

    corrected

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,49 @@
  *      }
  *
  */
+
+
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.createTable();
+  }
+  createTable() {
+  const table = document.createElement('table');
+  const thead = document.createElement('thead');
+  const tbody = document.createElement('tbody');
+
+
+const headRow = document.createElement('tr');
+Object.keys(this.rows[0]).forEach(column => {
+const headerCell = document.createElement('th');
+headerCell.textContent = column;
+headRow.appendChild(headerCell);
+});
+headRow.appendChild(document.createElement('th'));
+thead.appendChild(headRow);
+table.appendChild(thead);
+
+
+this.rows.forEach(rowData => {
+  const row = document.createElement('tr');
+  Object.values(rowData).forEach(value => {
+    const cell = document.createElement('td');
+    cell.textContent = value;
+    row.appendChild(cell);
+  });
+
+  const deleteCell = document.createElement('td');
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
+  deleteButton.addEventListener('click',() =>{
+    row.remove()
+  });
+  deleteCell.appendChild(deleteButton);
+  row.appendChild(deleteCell);
+  tbody.appendChild(row);
+});
+table.appendChild(tbody);
+return table;
   }
 }
